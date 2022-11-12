b6a2620d1d452ff223dbf7518a1dce6ca5f0c9b3
rosieposiecngl <c.aimyonngl@gmail.com>
2022-11-12 08:20:42 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+import 'pages/message_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+      ),
+      home: HomePage(),
+    );
+  }
+}
